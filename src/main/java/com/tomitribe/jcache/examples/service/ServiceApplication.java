b42c88694d101627f8199f68ff0b926c53d14418
Sebastian Daschner <git@sebastian-daschner.de>
2015-06-24 07:40:59 +0200

    updated Cache creation to use only JSR107 features; added Infinispan as possible JCache provider;

--- src/main/java/com/tomitribe/jcache/examples/service/ServiceApplication.java
+++ src/main/java/com/tomitribe/jcache/examples/service/ServiceApplication.java
@@ -17,15 +17,10 @@
 package com.tomitribe.jcache.examples.service;
 
 import javax.ws.rs.ApplicationPath;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 @ApplicationPath("/api")
 public class ServiceApplication extends javax.ws.rs.core.Application {
 
-    @Override
-    public Set<Class<?>> getClasses() {
-        return new HashSet<Class<?>>(Arrays.<Class<?>>asList(InterceptedService.class));
-    }
+    // no configuration needed
+
 }
